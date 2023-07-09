1b28f124badeeea34ce29a210f60c616f4e2b1e6
dcbell12 <136211163+dcbell12@users.noreply.github.com>
2023-07-09 01:16:47 -0400

    Create pytech_queries.py

---
+++ module_5/pytech_queries.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://admin:admin@cluster0.vxyt6xi.mongodb.net/?retryWrites=true&w=majority")
+
+db = client["PyTech"]
+
+students = db["students"]
+
+docs = students.find({})
+for doc in docs:
+    print(doc)
+
+student = students.find_one({"student_id": "1007"})
+print(student)
